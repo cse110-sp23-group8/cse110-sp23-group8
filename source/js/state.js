2437e30225d84fe61a2b876453e5aec156fa1d6d
Marcelo Shen <caogangshen@gmail.com>
2023-06-07 23:34:39 -0700

    fix fetch problem

--- source/js/state.js
+++ source/js/state.js
@@ -1,7 +1,102 @@
 import gameObject from "./fortunes.js";
-const nutritions = await fetch("source/data/nutrition.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
+const nutritions = {
+  "Chow Mein": {
+      "calories": 500,
+      "fat": 23,
+      "carbohydrates": 61,
+      "protein": 18,
+      "sugars": 5,
+      "sodium": 980
+  },
+  "Fried Rice": {
+      "calories": 530,
+      "fat": 16,
+      "carbohydrates": 83,
+      "protein": 11,
+      "sugars": 2,
+      "sodium": 780
+  },
+  "White Rice": {
+      "calories": 380,
+      "fat": 4.5,
+      "carbohydrates": 82,
+      "protein": 7,
+      "sugars": 0,
+      "sodium": 0
+  },
+  "Black Pepper Angus Steak": {
+      "calories": 220,
+      "fat": 8,
+      "carbohydrates": 10,
+      "protein": 29,
+      "sugars": 5,
+      "sodium": 810
+  },
+  "Broccoli Beef": {
+      "calories": 150,
+      "fat": 7,
+      "carbohydrates": 16,
+      "protein": 7,
+      "sugars": 4,
+      "sodium": 440
+  },
+  "Beijing Beef": {
+      "calories": 470,
+      "fat": 23,
+      "carbohydrates": 39,
+      "protein": 24,
+      "sugars": 18,
+      "sodium": 1070
+  },
+  "Eggplant Tofu": {
+      "calories": 190,
+      "fat": 10,
+      "carbohydrates": 18,
+      "protein": 8,
+      "sugars": 6,
+      "sodium": 880
+  },
+  "Kung Pao Chicken": {
+      "calories": 290,
+      "fat": 16,
+      "carbohydrates": 19,
+      "protein": 14,
+      "sugars": 13,
+      "sodium": 810
+  },
+  "Mushroom Chicken": {
+      "calories": 260,
+      "fat": 11,
+      "carbohydrates": 15,
+      "protein": 27,
+      "sugars": 2,
+      "sodium": 590
+  },
+  "The Original Orange Chicken": {
+      "calories": 380,
+      "fat": 18,
+      "carbohydrates": 44,
+      "protein": 18,
+      "sugars": 17,
+      "sodium": 620
+  },
+  "Sizzling Shrimp": {
+      "calories": 150,
+      "fat": 9,
+      "carbohydrates": 8,
+      "protein": 10,
+      "sugars": 4,
+      "sodium": 280
+  },
+  "Honey Walnut Shrimp": {
+      "calories": 360,
+      "fat": 19,
+      "carbohydrates": 36,
+      "protein": 13,
+      "sugars": 14,
+      "sodium": 240
+  }
+};
 let game;
 let level;
 let click = false;
@@ -101,7 +196,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
         let element = document.querySelector(`#option-${i}-${j}`);
         let dishName = element.textContent.trim();
-        game.cumulateNutritions(nutritions[dishName]);
+        game.cumulateNutritions(nutritions[dishName])
         choiceAudio();
         hideAllPages();
         if (i === level) {
