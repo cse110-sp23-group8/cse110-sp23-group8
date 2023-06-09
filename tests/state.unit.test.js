e67ee16e60ae13769c0f3ce4242d5b5f7213a517
jackmontoro <jmontoro@ucsd.edu>
2023-06-09 08:22:56 -0700

    add hideAllPages test

--- tests/state.unit.test.js
+++ tests/state.unit.test.js
@@ -1,7 +1,12 @@
 const values = require("../source/js/state.js");
 
 // Test hideAllPages
-
+test("Function should hide every page", () => {
+    values.hideAllPages();
+    pages.array.forEach((page) => {
+        expect(document.getElementById(page).style.display).toBe("none");
+    });
+});
 // Test addClickEvent
 
 // Test showMessage
